a0e7df59034a501c4ea196072a456bff9ceb7b98
Marcos Basurto <marcosbr301@gmail.com>
2020-11-29 18:50:32 -0500

    co

--- src/main/java/intercorp/com/pe/springboot/app/clientes/models/config/SwaggerConfig.java
+++ src/main/java/intercorp/com/pe/springboot/app/clientes/models/config/SwaggerConfig.java
@@ -1,13 +1,11 @@
 package intercorp.com.pe.springboot.app.clientes.models.config;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.web.bind.annotation.RestController;
 import springfox.documentation.builders.PathSelectors;
 import springfox.documentation.builders.RequestHandlerSelectors;
 import springfox.documentation.spi.DocumentationType;
 import springfox.documentation.spring.web.plugins.Docket;
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
-import com.google.common.base.Predicates;
 
 
 @Configuration
